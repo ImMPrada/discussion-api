f6736b5ab098f350c76c351a9267c360342edca9
ImMPrada <im.mprada@gmail.com>
2022-04-22 17:13:35 -0500

    Add registrarion and session

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,12 +311,12 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{^/sign_in$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/sign_out$}]
-    ]
-    jwt.expiration_time = 1.day.to_i
+    # jwt.dispatch_requests = [
+    #   ['POST', %r{^/login$}]
+    # ]
+    # jwt.revocation_requests = [
+    #   ['DELETE', %r{^/logout$}]
+    # ]
+    # jwt.expiration_time = 1.day.to_i
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,11 @@ Rails.application.routes.draw do
   
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users,
+            defaults: { format: :json }
   
   namespace :api do
     resources :posts
     resources :reactions, only: [:create]
-    devise_for :users,
-               path: '',
-               path_names: {
-                 sign_in: 'login',
-                 sign_out: 'logout',
-                 registration: 'signup'
-               },
-               controllers: {
-                 sessions: 'sessions',
-                 registrations: 'registrations'
-               }
   end
 end
